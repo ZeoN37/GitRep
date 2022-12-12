1d77bffb8bc467ff6db52b2fd3781426b403ff3b
Макс <max_37rus@inbox.ru>
2022-12-12 21:49:28 +0300

    ок

--- Probe.py
+++ Probe.py
@@ -1,4 +1,27 @@
-print('Hello World!')
-user_name = input('Привет! Как тебя зовут! ')
-print(f'Как дела {user_name}?')
-print(user_name)
+number_1 = 3
+number_2 = 6
+
+while True:
+    operation = input('Bедите операцию:')
+    try:
+        operation == '+'
+        result = number_1 + number_2
+        print(f'{number_1}+{number_2}={result}')
+        break
+
+    except ValueError:
+        try:
+            operation == '-'
+            result = number_1 - number_2
+            print(f'{number_1}-{number_2}={result}')
+            break
+
+        except ValueError:
+            try:
+                operation == '*'
+                result = number_1 * number_2
+                print(f'{number_1}*{number_2}={result}')
+                break
+
+            except ValueError:
+                print("Неверное значение. Bедите второе число: ")
---
+++ task_1.py
@@ -0,0 +1,56 @@
+
+while True:
+    number_1 = input('Bедите первое число: ')
+    try:
+        number_1 = int(number_1)
+        print("Первое число: ", number_1)
+        break
+    except ValueError:
+        try:
+            float(number_1)
+            print("Первое число: ", number_1)
+            break
+        except ValueError:
+            print("Неверное значение. Bедите первое число: ")
+
+while True:
+    number_2 = input('Bедите второе число: ')
+    try:
+        number_2 = int(number_2)
+        print("Второе число: ", number_2)
+        break
+    except ValueError:
+        try:
+            float(number_2)
+            print("Второе значение: ", number_2)
+            break
+        except ValueError:
+            print("Неверное значение. Bедите второе число: ")
+
+while True:
+    operation = input('Bедите операцию: ')
+    try:
+        if operation == '+':
+            result = number_1 + number_2
+            print(f'{number_1}+{number_2}={result}')
+            break
+        if operation == '-':
+            result = number_1 - number_2
+            print(f'{number_1}-{number_2}={result}')
+            break
+        if operation == '*':
+            result = number_1 * number_2
+            print(f'{number_1}*{number_2}={result}')
+            break
+        if operation == '/':
+            result = number_1 / number_2
+            print(f'{number_1}/{number_2}={result}')
+            break
+        else:
+            print("Неверное значение. Bедите операцию: ")
+    except ValueError:
+        continue
+
+
+
+
